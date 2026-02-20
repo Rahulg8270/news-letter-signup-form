6d559a104c1e7ab9655b3dd1529818f1a774100a
Rahul G <rahul.g8270@gmail.com>
2026-02-20 08:16:58 +0530

    logic for email validation and styling for modal

--- index.js
+++ index.js
@@ -1,67 +1,34 @@
-// const form = document.querySelector("form");
-// const btnSubmit = document.querySelector("button");
-// const email = document.querySelector('input[type="email"]');
-// const emailError = document.querySelector("span.error");
-// const emailSubscribed = document.querySelector(".emailSubscribed");
-// // const modal = document.querySelector(".success-modal");
-// const btnDismiss = document.querySelector(".btn-dismiss");
-// const mainContent = document.getElementById("main-content");
-// const modalWrapper = document.getElementById("modal-wrapper");
-// console.log(emailError.textContent);
+const newsletterForm = document.querySelector(".newsletter__form");
+const emailErrorLabel = document.querySelector(".newsletter__label-error");
+const emailInput = document.querySelector(".newsletter__input");
+const successModal = document.querySelector("#success-modal");
+const closeModalBtn = document.querySelector(".modal__close-btn");
+const modalEmailDisplay = document.querySelector(".modal__email-display");
 
-// form.addEventListener("submit", submitHandler);
+emailInput.addEventListener("input", (event) => validateEmail(event));
 
-// function submitHandler(e) {
-//   e.preventDefault();
-
-//   if (!email.validity.valid) {
-//     console.log("it is a invalid email");
-//     modalWrapper.classList.remove("open-modal");
-//     modalWrapper.classList.add("close-modal");
-//     showError();
-//   } else {
-//     const formData = new FormData(e.target);
-//     // const emailData = formData.get("email");
-//     const finalData = Object.fromEntries(formData.entries());
-//     // console.log("getting the data", finalData.email);
-//     emailSubscribed.textContent = finalData.email;
-//     modalWrapper.classList.add("open-modal");
-//     modalWrapper.classList.remove("close-modal");
-//     mainContent.classList.add("blur-background");
-//     modalWrapper.classList.remove("hidden");
-//   }
-// }
-// btnDismiss.addEventListener("click", () => {
-//   modalWrapper.classList.add("close-modal");
-//   mainContent.classList.remove("blur-background");
-// });
-
-// email.addEventListener("input", (event) => {
-//   if (email.validity.valid) {
-//     emailError.textContent = "";
-
-//     // emailError.className = "error";
-//   } else {
-//     showError();
-//   }
-// });
-
-// function showError() {
-//   if (email.validity.valueMissing) {
-//     emailError.textContent = "You need to enter a valid email address";
-//   } else if (email.validity.tooShort) {
-//     emailError.textContent = `email needs to be atleast 8 characters`;
-//   } else if (email.validity.typeMismatch) {
-//     emailError.textContent = `type the correct email format @som.com`;
-//   }
-//   emailError.className = "error";
-// }
-
-const modal = document.querySelector("#newsletter-modal");
-const openBtn = document.querySelector(".newsletter__submit-btn");
-
-openBtn.addEventListener("submit", (event) => {
+newsletterForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("clicked the submit btn");
-  modal.showModal();
+  const formData = new FormData(event.target);
+
+  const userEmail = formData.get("email");
+
+  modalEmailDisplay.textContent = userEmail;
+
+  if (emailInput.checkValidity()) {
+    successModal.showModal();
+  }
+});
+
+function validateEmail(event) {
+  console.log(event);
+}
+
+closeModalBtn.addEventListener("click", (event) => {
+  successModal.close();
+  newsletterForm.reset();
+});
+
+successModal.addEventListener("close", () => {
+  newsletterForm.reset();
 });
